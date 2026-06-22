558705087e0239db15a7c1df5de3b53aa6e46211
verdantTch <verdant318@gmail.com>
2026-06-22 12:57:59 +0200

    Correction bugs fonctionnels annotation manuelle
    Ne pas permettre l'enregistrement d'un point si en dehors de la zone de l'image.
    Ne pas permettre le clic avant chargement d'un dossier image (et donc d'une image).
    Correction de bug d'affichage de bannière à chaque changement de dossier.
    
    Dernière sauvegarde avant modification importante et ajout des menus.
    Création des deux menus :
    - Annotion manuelle et vérification
    - Traitement IA

---
+++ Zannote/ui/ai_menu.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun 22 12:41:08 2026
+
+@author: hugoz
+"""
+
---
+++ Zannote/ui/home_page.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun 22 12:40:09 2026
+
+@author: hugoz
+"""
+
