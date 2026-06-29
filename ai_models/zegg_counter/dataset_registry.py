9c540fcd40580d961c8e309904b68f49800295b6
verdantTch <verdant318@gmail.com>
2026-06-29 14:53:13 +0200

    Déletion des fichiers de testing
    Après premier entraînement réussi du modèle

--- ai_models/zegg_counter/dataset_registry.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Tue Jun 23 10:05:49 2026
-
-@author: hugoz
-"""
-
--- ai_models/zegg_counter/model_registry.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Tue Jun 23 10:05:22 2026
-
-@author: hugoz
-"""
-
